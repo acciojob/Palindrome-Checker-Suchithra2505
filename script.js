d4715ebe2bac883b108dde5aaca7e8070ebd88e1
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-05-28 16:23:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
 function palindrome(str){
+const normalizedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+  // Compare the normalized string with its reverse
+  const reversedStr = normalizedStr.split('').reverse().join('');
+  
+  return normalizedStr === reversedStr;
 }
 module.exports = palindrome
